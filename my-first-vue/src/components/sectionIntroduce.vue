1dc9cdf3500980ea32debb190a5b842b231eb32d
hatong <hatong@cisco.com>
2018-06-26 15:39:10 +0800

    my vue project

---
+++ my-first-vue/src/components/sectionIntroduce.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="container sky-container">
+
+        <div class="row unsalable">
+            <div class="col-sm-5">
+                <a class="videoimg" target="_blank" ><img class="video-img" src="../images/video.png"></a>
+            </div>
+            <div class="col-sm-7 text-wrapper-sky ">
+                <h2 class="sky">
+                    苍天厚土：中国菜品滞销背后的现状
+                </h2>
+                <div class="sky-describe">
+                    <p>农产品滞销，是中国农民的一根软肋。</p>
+                    <p>经历一个四季轮回，付出一年辛勤劳作，他们希望能在秋天丰收，收获一家人的生计。却因为菜品滞销，打破最美好的夙愿。有的生活，因为滞销，戛然而止。</p>
+                    <p>中国近两年以来蔬菜的滞销，到底严重到什么程度？滞销的背后，又有哪些链条层层剥削，却不可或缺？中国农民遭遇滞销，是如何的苦不堪言？</p>
+                    <p>《苍天厚土》讲述在传统的农产品流通链条下，因为菜品滞销，中国农民的真实现状。</p>
+                </div>
+                <a target="_blank"><div class="sky-video" >腾讯视频：《苍天厚土》完整版视频 <span class="to-href-icon"> &gt;&gt; </span></div></a>
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+    .sky-container {
+        padding-left: 0px !important;
+        padding-right: 0px !important;
+    }
+    .unsalable {
+        margin-top: 82px;
+        margin-bottom: 68px;
+    }
+    .text-wrapper-sky {
+        height: 100%;
+        padding-left: 40px;
+    }
+    .sky {
+        font-size: 24px;
+        margin-top: 30px;
+        color: #383838;
+    }
+    .sky-describe {
+        color: rgb(170,170,170);
+        font-size: 14px;
+        margin-top: 20px;
+    }
+    .to-video {
+        margin-top: 26px;
+        color: #383838;
+        font-size: 16px;
+    }
+    .to-href-icon {
+        color: rgb(190,190,190);
+    }
+    .video-img {
+        width: 100%;
+        height: auto;
+        height: 300px;
+        width: 460px;
+    }
+    .unsalable {
+        margin-top: 82px;
+        margin-bottom: 68px;
+    }
+</style>
